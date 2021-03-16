79fddb6f014ca6e5bf8164632726644c547c265f
Justin C Louis <justin@dataspacepublishing.com>
2021-03-15 21:42:45 -0500

    Implement postCSS processing, etc Muchly improved the SCSS/CSS processing pipeline. For now, does not differentiate between prod and dev.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
-}
+  plugins: {
+      '@fullhuman/postcss-purgecss': {
+          content: [
+              './layouts/**/*.html',
+              './static/js/*.js',
+              './assets/js/*.js'
+            ],
+          whitelist: [
+              'h3',
+              'h4',
+              'ul',
+              'li'
+          ]
+      },
+      autoprefixer: {},
+      cssnano: {preset: 'default'}
+  }
+};
