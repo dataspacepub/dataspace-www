a81cde09526043ded1d822f271eb343651d3448d
Justin C Louis <justin@dataspacepublishing.com>
2021-04-04 12:52:24 -0500

    Proper dev/prod environment seperation

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,27 @@
+/* configure PurgeCSS to process the stat file, and safelist a few elements */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [ './hugo_stats.json' ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    },
+    safelist: [
+      'h3',
+      'h4',
+      'ul',
+      'li',
+      'float-left',
+      'float-right'
+    ]
+});
+
+/* disable PurgeCSS during development runs */
 module.exports = {
-  plugins: {
-      '@fullhuman/postcss-purgecss': {
-          content: [
-              './layouts/**/*.html',
-              './static/js/*.js',
-              './assets/js/*.js'
-            ],
-            safelist: [
-              'h3',
-              'h4',
-              'ul',
-              'li',
-              'float-left',
-              'float-right'
-          ]
-      },
-      autoprefixer: {},
-      cssnano: {preset: 'default'}
-  }
-};
+  plugins: [
+    require('autoprefixer'),
+    ...process.env.HUGO_ENVIRONMENT === 'production'
+      ? [purgecss]
+      : []
+  ],
+    cssnano: {preset: 'default'}
+}
